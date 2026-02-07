7dd706f611c0b8417740b836da71f68dd4c1558f
kemfo4 <kemfomate@gmail.com>
2026-02-08 00:05:50 +0100

    Fixing design

--- app/src/main/java/com/example/kostkiv2/MainActivity.kt
+++ app/src/main/java/com/example/kostkiv2/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.kostkiv2
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,9 +20,23 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        var balanceView: TextView = findViewById(R.id.Balance);
+        balanceView.text = getString(R.string.balance, balance)
     }
+
+    val balance: Int =100;
 
     fun loadGames() {
         val gamesHub: LinearLayout = findViewById(R.id.gamesHub);
+    }
+
+    fun rollDice(view: View) {
+
+    }
+
+    fun rollDices(view: View) {
+        val dicesDraw: Array<Int> = arrayOf(R.drawable.k1, R.drawable.k2, R.drawable.k3, R.drawable.k4, R.drawable.k5, R.drawable.k6);
+
     }
 }
