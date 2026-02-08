cfc1b9e22f1041cc1ae97fc104774cc35079f965
kemfo4 <kemfomate@gmail.com>
2026-02-08 20:22:58 +0100

    Adding Functionality and Fixing Design Flaws

--- app/src/main/java/com/example/kostkiv2/MainActivity.kt
+++ app/src/main/java/com/example/kostkiv2/MainActivity.kt
@@ -1,14 +1,17 @@
+@file:Suppress("UNCHECKED_CAST")
+
 package com.example.kostkiv2
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.widget.LinearLayout
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.lang.Integer.parseInt
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,22 +24,74 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        var balanceView: TextView = findViewById(R.id.Balance);
-        balanceView.text = getString(R.string.balance, balance)
+        val balanceText: TextView = findViewById(R.id.textBalance)
+        val scoreText: TextView = findViewById(R.id.textScore)
+        dicesDraw = arrayOf(R.drawable.k1, R.drawable.k2, R.drawable.k3, R.drawable.k4, R.drawable.k5, R.drawable.k6)
+        balanceText.text = getString(R.string.balance, balance)
+        scoreText.text = getString(R.string.score, score)
     }
 
-    val balance: Int =100;
-
-    fun loadGames() {
-        val gamesHub: LinearLayout = findViewById(R.id.gamesHub);
-    }
+    var balance: Int = 100
+    var score: Int = 0
+    var dicesDraw = arrayOfNulls<Int>(6) as Array<Int>
 
     fun rollDice(view: View) {
+        val selDice: ImageButton = view as ImageButton
+
+        if((balance-2)>=0 && selDice.contentDescription.toString()!="%d" && selDice.contentDescription.toString()!="1") {
+            val value = Random.nextInt(1, 6 + 1)
+            score -= parseInt(selDice.contentDescription.toString())
+
+            selDice.setImageResource(dicesDraw[value - 1])
+            selDice.contentDescription = getString(R.string.value, value)
+
+            score+= parseInt(selDice.contentDescription.toString())/2
+        }
 
     }
 
     fun rollDices(view: View) {
-        val dicesDraw: Array<Int> = arrayOf(R.drawable.k1, R.drawable.k2, R.drawable.k3, R.drawable.k4, R.drawable.k5, R.drawable.k6);
+        val balanceText: TextView = findViewById(R.id.textBalance)
+        val scoreText: TextView = findViewById(R.id.textScore)
 
+        val dices: Array<ImageButton> = arrayOf(findViewById(R.id.dice1),
+                                                findViewById(R.id.dice2),
+                                                findViewById(R.id.dice3),
+                                                findViewById(R.id.dice4),
+                                                findViewById(R.id.dice5))
+
+        score = 0
+        balance-=5
+
+        for(dice in dices) {
+            val value = Random.nextInt(1, 6+1)
+
+            dice.setImageResource(dicesDraw[value-1])
+            dice.contentDescription = getString(R.string.value, value)
+        }
+
+        {
+            var index = 1
+            var multiplier = 1
+            while (index < 5) {
+                if (dices[index - 1].contentDescription == dices[index].contentDescription)
+                    multiplier++
+                else if (multiplier >= 2) {
+                    score += parseInt(dices[index - 1].contentDescription.toString()) * multiplier
+                    multiplier = 1
+                }
+
+                index++
+            }
+
+            if(multiplier>=2)
+                score += parseInt(dices[index - 1].contentDescription.toString()) * multiplier
+        }
+
+        if(score>=10)
+            balance+=score/2
+
+        balanceText.text = getString(R.string.balance, balance)
+        scoreText.text = getString(R.string.score, score)
     }
 }
